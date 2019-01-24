f9bf2582ea0d490e82020960cc598c021d06dab5
Fantix King <fantix@uchicago.edu>
2019-01-24 13:32:33 -0600

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cdispyutils",
-    version="0.2.0",
+    version="0.3.0",
     description="General utilities for Gen3 development",
     license="Apache",
     install_requires=[
